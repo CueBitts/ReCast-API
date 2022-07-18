145a718d36084089e1b64ea320bcfa844117dba5
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-07-18 16:31:02 -0700

    API fixes

--- recast/serializers.py
+++ recast/serializers.py
@@ -2,21 +2,7 @@ from rest_framework import serializers
 from .models import User, Recast, RecastInst, Actor, Movie, CastInst
 
 
-class RecastInstSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RecastInst
-        fields = ['id', 'recast', 'name', 'actor', 'upvotes', 'downvotes']
-
-class RecastSerializer(serializers.ModelSerializer):
-    recastInsts = RecastInstSerializer(many=True, read_only=True)
-    
-    class Meta:
-        model = Recast
-        fields = ['id', 'name', 'movie', 'user', 'desc', 'recastInsts', 'upvotes', 'downvotes']
-
 class UserSerializer(serializers.ModelSerializer):
-    recasts = RecastSerializer(many=True, read_only=True)
-    
     class Meta:
         model = User
         fields = ['id', 'name', 'password', 'recasts']
@@ -27,17 +13,39 @@ class CastInstSerializer(serializers.ModelSerializer):
         fields = ['id', 'movie', 'name', 'actor']
 
 class ActorSerializer(serializers.ModelSerializer):
-    recastInsts = RecastInstSerializer(many=True, read_only=True)
     castInsts = CastInstSerializer(many=True, read_only=True)
     
     class Meta:
         model = Actor
         fields = ['id', 'tmdbid', 'name', 'img', 'recastInsts', 'castInsts']
 
+class RecastInstSerializer(serializers.ModelSerializer):
+    actor = ActorSerializer(read_only=True)
+    
+    class Meta:
+        model = RecastInst
+        fields = ['id', 'recast', 'name', 'actor', 'upvotes', 'downvotes']
+
 class MovieSerializer(serializers.ModelSerializer):
     castInsts = CastInstSerializer(many=True, read_only=True)
-    recasts = RecastSerializer(many=True, read_only=True)
     
     class Meta:
         model = Movie
         fields = ['id', 'tmdbid', 'name', 'img', 'castInsts', 'recasts']
+
+class RecastSerializer(serializers.ModelSerializer):
+    class Meta:
+        movie = MovieSerializer(read_only=True)
+        user = UserSerializer(read_only=True)
+        recastInsts = RecastInstSerializer(many=True, read_only=True)
+        
+        model = Recast
+        fields = ['id', 'name', 'movie', 'user', 'desc', 'recastInsts', 'upvotes', 'downvotes']
+        depth = 2
+
+
+
+
+
+
+
