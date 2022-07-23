e3705c4b96377da078c7f1ed30d3c8c41dda98f2
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-07-22 23:23:45 -0700

    deploy 20

--- recast/settings.py
+++ recast/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'recast/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
